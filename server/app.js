ac61f87d4a500d298f0abf1ecc493c8ec32d7985
macklevine <mackplevine@gmail.com>
2017-01-07 00:01:53 -0800

    serving webpage

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 
 import express from "express";
 import http from "http";
+import bodyParser from "body-parser";
 import {server as serverConfig} from './config/index';
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/', express.static(__dirname + '/../client'));
+
+
 const server = http.createServer(app);
 
 server.listen(serverConfig.port, function(){
